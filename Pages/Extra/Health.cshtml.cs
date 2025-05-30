dda8be6e06cc865a22c7011fc6268d4533187c9c
nickpreston24 <nickpreston24@gmail.com>
2025-05-30 14:08:21 -0400

    ++ kanband and Health page

---
+++ Pages/Extra/Health.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Drip.UI.Pages.Extra;
+
+public class Health : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
---
+++ experimental/Kanban.cshtml.cs
@@ -0,0 +1,10 @@
+using Hydro;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Drip.UI;
+
+[HtmlTargetElement("sample-kanban")]
+public class Kanban : HydroView
+{
+    // your props here
+}
